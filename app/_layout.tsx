7d450af987ce854d793845f5194240609814b1d3
DavidAsrorxonov <dovudxon.asrorxonov@icloud.com>
2026-03-29 02:00:48 +0900

    feat: implement splash screen hiding upon font loading in root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import "@/global.css";
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
@@ -11,6 +12,14 @@ export default function RootLayout() {
     "sans-extrabold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
     "sans-light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
   });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
 
   return <Stack screenOptions={{ headerShown: false }} />;
 }
